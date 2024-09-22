7dfa7dc434b0cb5dcececf90c0035a966c1fc77c
Thomas <cbakewell1@gmail.com>
2024-09-22 13:57:15 -0400

    minor refactoring

--- utils/crc.h
+++ utils/crc.h
@@ -13,6 +13,8 @@ typedef struct{
 
 unsigned int generateChecksum(char* text);
 
+void testCombos(char* input, char* ascii, int inputlen, int curr, int max, int target, args* passed);
+
 void* crackSum(void* args);
 
 void matchSums(char* inputText, unsigned int target);
